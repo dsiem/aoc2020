f620468debbca534ce51174ab5ac3f2ec19a1d83
Daniel Siemssen <daniel@dr-siemssen.de>
2020-12-09 21:38:23 +0100

    Use sum instead of mapreduce

--- 06.jl
+++ 06.jl
@@ -2,8 +2,8 @@ function day06(file="06.input")
     input = split(read(file, String), "\n\n")
 
     # Part 1
-    mapreduce(length ∘ unique ∘ join ∘ split, +, input) |> println
+    sum(length ∘ unique ∘ join ∘ split, input) |> println
 
     # Part 2
-    mapreduce(length ∘ (x -> intersect(x...)) ∘ split, +, input) |> println
+    sum(length ∘ (x -> intersect(x...)) ∘ split, input) |> println
 end
