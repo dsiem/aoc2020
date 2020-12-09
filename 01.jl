5cec088ff8c13779e1d06814b1880272d11a1b08
Daniel Siemssen <daniel@dr-siemssen.de>
2020-12-09 22:17:27 +0100

    Harmonize style

--- 01.jl
+++ 01.jl
@@ -4,7 +4,7 @@ function day01(file="01.input")
     input = parse.(Int, readlines(file))
 
     # Part 1 & 2
-    for n in [2,3]
+    for n ∈ [2,3]
         Iterators.filter(xs -> sum(xs) == 2020, combinations(input, n)) |> first |> prod |> println
     end
 end
