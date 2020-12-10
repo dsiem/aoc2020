debfed7323af3749c1e4316d779ab1efc86e1104
Daniel Siemssen <daniel@dr-siemssen.de>
2020-12-10 16:48:59 +0100

    Solution for day 10

---
+++ 10.jl
@@ -0,0 +1,22 @@
+using IterTools
+
+function day10(file="10.input")
+    input = parse.(Int, readlines(file)) |> sort
+    prepend!(input, 0)
+    append!(input, input[end]+3)
+    diffs = diff(input)
+    
+    # Part 1
+    prod(d -> count(diffs .== d), [1, 3]) |> println
+
+    # Part 2 (use that differences are 1 or 3, never 2)
+    prod(first(g) != 1 || trib(length(g)+2) for g ∈ groupby(first, diffs)) |> println
+end
+
+# Tribonacci numbers
+# trib(n+2) = number of compositions of n with parts 1, 2 and 3
+function trib(n::Integer)
+    n < 2  && return 0
+    n == 2 && return 1 
+    return trib(n-3) + trib(n-2) + trib(n-1)
+end
