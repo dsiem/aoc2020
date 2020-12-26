1166700a572cbfbde17c7ba9398cce51163ecccd
Daniel Siemssen <daniel@dr-siemssen.de>
2020-12-26 10:35:03 +0100

    Improve day 5

--- 05.jl
+++ 05.jl
@@ -1,5 +1,5 @@
 function day05(file="05.input")
-    input = map(line -> parse(Int, map(c -> c ∈ ['B', 'R'] ? '1' : '0', line), base=2), eachline(file))
+    input = map(line -> parse(Int, map(c -> c ∈ ('B', 'R') ? '1' : '0', line), base=2), eachline(file))
 
     part1 = maximum(input)
     part2 = setdiff(minimum(input):maximum(input), input) |> first
