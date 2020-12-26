a5a76a846cbbe9dd922f2989451b73cac65f84fe
Daniel Siemssen <daniel@dr-siemssen.de>
2020-12-26 10:36:51 +0100

    Reinsert empty line at end of file

--- 06.jl
+++ 06.jl
@@ -1,5 +1,5 @@
 function day06(file="06.input")
-    input = split(read(file, String), "\n\n")
+    input = split(read(file, String) |> strip, "\n\n")
 
     part1 = sum(length ∘ unique ∘ join ∘ split, input)
     part2 = sum(length ∘ (x -> intersect(x...)) ∘ split, input)
